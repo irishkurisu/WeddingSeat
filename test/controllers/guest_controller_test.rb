3ffa376b3fbb8d0368cfa47c050ac3b0a2087630
Chris Cunningham <irishccc@gmail.com>
2018-08-08 21:58:54 -0600

    Remove old test dir

--- test/controllers/guest_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class GuestControllerTest < ActionDispatch::IntegrationTest
-  test "should get create,read,update,delete" do
-    get guest_create,read,update,delete_url
-    assert_response :success
-  end
-
-end
